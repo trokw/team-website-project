a4703f3f809289d3b37018ec7156eef77cc88b5a
maulet56 <hamitmaulet229@gmail.com>
2025-11-06 13:52:53 +0600

    Add files via upload

---
+++ feature.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("clickBtn");
+  button.addEventListener("click", () => {
+    alert("Сіз батырманы бастыңыз! 🚀");
+  });
+});
